241fe926dbd97d91c238dc19ea61d742f68ddf48
Kelsi Olstad <kelsiolstad@gmail.com>
2022-02-15 19:35:35 -0700

    refactor

--- timer1.js
+++ timer1.js
@@ -1,22 +1,11 @@
-
-//No numbers are provided: Easy. It should just not beep at all and end immediately since no beeps should get scheduled.
-
-//An input is a negative number: Ignore/skip any numbers that are negative. We can't schedule anything in the past.
-
-//An input is not a number: Ignore/skip these as well, instead of attempting to call setTimeout with a non-number.
-
-const args = process.argv.slice(2);
-
-  for(const arg of args){
-    console.log(typeof arg);
-    const num = parseInt(arg);
-    if(arg === ""){
-      process.exit(); //works!
-    }
-    if(num < 0 || typeof num !== "number"){
-      continue;
-    }
+const args = process.argv.slice(2).filter(number => {
+  return number > 0;
+});
+for (const arg of args) {
+  if (arg === "") {
+    process.exit(); //works!
+  }
   setTimeout(() => {
     process.stdout.write("<3");
   }, arg * 1000);
-};
+}
